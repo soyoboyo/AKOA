c4b3753891236e617f076f2d2f766932de201858
tubidubidam <jacekzalewski95@gmail.com>
2017-10-04 20:06:47 +0200

    rotation complete

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] test = {1, 1, 2, 3, 3, 5, 7, 7, 5, 2, 4, 3, 3};
-        System.out.println("number without pair: " + strangerNumber(test));
+//        int[] test = {1, 1, 2, 3, 3, 5, 7, 7, 5, 2, 4, 3, 3};
+//        System.out.println("number without pair: " + strangerNumber(test));
 
+
+        int[] rotest = {3, 8, 9, 7, 6};
+        rotateArray(rotest, 3);
     }
 
     public static int binaryGap(int N) {
@@ -24,6 +27,7 @@ public class Main {
         return 0;
     }
 
+    // https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
     public static int strangerNumber(int []A) {
 
         Arrays.sort(A);
@@ -40,5 +44,24 @@ public class Main {
             }
         }
         return 0;
+    }
+
+    private static int[] rotateArray(int[] A, int K) {
+
+        int last = A.length - 1;
+        int tmp = A[last];
+
+        for (int j = 0; j < K; j++) {
+            for (int i = last; i > 0; i--) {
+                A[i] = A[i-1];
+            }
+            A[0] = tmp;
+           tmp = A[last];
+        }
+        for (int i = 0; i < A.length; i++) {
+            System.out.println("A[" + i + "] = " + A[i]);
+        }
+
+        return A;
     }
 }
