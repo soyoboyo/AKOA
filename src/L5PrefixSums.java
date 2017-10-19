191764c21650586421d35f28ff9523a3a23e1566
tubidubidam <jacekzalewski95@gmail.com>
2017-10-19 15:10:45 +0200

    prefix sums n slices

--- src/L5PrefixSums.java
+++ src/L5PrefixSums.java
@@ -11,9 +11,9 @@ public class L5PrefixSums {
                 10, 10, 5,
                 10, 10, 7,
                 10, 10, 20};
-        int[] countDivTestAns = {3, 1, 20, 1, 1, 1};
+        int[] countDivTestAns = {3, 1, 20, 1, 0, 0};
 
-        for (int i = 0, j = 0; j < countDivTestAns.length - 1; i += 3, j++) {
+        for (int i = 0, j = 0; j < countDivTestAns.length ; i += 3, j++) {
             System.out.println(
                     countDividers(countDivsTest[i], countDivsTest[i + 1], countDivsTest[i + 2]) == countDivTestAns[j]
             );
@@ -22,13 +22,12 @@ public class L5PrefixSums {
     }
 
     public static int countDividers(int a, int b, int k) {
-        int count;
-        count = (b - a);
-        int divs = count / k;
-        if (a == b && a%k == 0) {
+        int divs = (b - a) / k;
+        if (a % k == 0 || b % k == 0) {
+            divs++;
+        } else if (a < k && b > k) {
             divs++;
         }
-
         return divs;
     }
 }
