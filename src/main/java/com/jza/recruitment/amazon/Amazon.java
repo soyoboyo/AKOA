58e570e50d9be4b4bfaa3956c6ebddf622db6400
Jacek <jacekzalewski95@gmail.com>
2019-05-19 16:33:56 +0200

    Solved 6/7 tasks, using assertArrayEquals for array tests

--- src/main/java/com/jza/recruitment/amazon/Amazon.java
+++ src/main/java/com/jza/recruitment/amazon/Amazon.java
@@ -2,25 +2,23 @@ package com.jza.recruitment.amazon;
 
 public class Amazon {
 
-	public static int[] reverseArray(int arr[]){
-		int i, temp, originalLen = arr.length;
-		int len = originalLen;
-		for(i = 0; i < originalLen / 2; i++){
-			temp = arr[len -1];
-			arr[len - 1] = arr[i];
-			arr[i] = temp;
-			len += 1;
+	public static int[] reverseArray(int[] arr) {
+		int i, temp, len = arr.length;
+		for (i = 0; i < len / 2; i++) {
+			temp = arr[i];
+			arr[i] = arr[len - i - 1];
+			arr[len - i - 1] = temp;
 		}
 		return arr;
 	}
 
-	public static int[] sortArray(int arr[]){
+	public static int[] sortArray(int[] arr) {
 		int i, max, location, j, temp, len = arr.length;
-		for(i = 0; i < len; i++){
+		for (i = 0; i < len; i++) {
 			max = arr[i];
 			location = i;
-			for(j = i; j < len; j++){
-				if(max > arr[j]){
+			for (j = i; j < len; j++) {
+				if (max > arr[j]) {
 					max = arr[j];
 					location = j;
 				}
@@ -32,57 +30,57 @@ public class Amazon {
 		return arr;
 	}
 
-	public static int[] replaceValues(int arr[]){
+	public static int[] replaceValues(int[] arr) {
 		int i, j, len = arr.length;
-		if(len % 2 == 0){
-			for(i = 0; i <= len; i++)
+		if (len % 2 == 0) {
+			for (i = 0; i < len; i++)
 				arr[i] = 0;
 		} else {
-			for(j = 0; j<= len; j++)
-			arr[j] = 1;
+			for (j = 0; j < len; j++)
+				arr[j] = 1;
 		}
 		return arr;
 	}
 
-	public static int countOccurrence(int arr[], int value){
+	public static int countOccurrence(int[] arr, int value) {
 		int i = 0, count = 0, len = arr.length;
-		while (i < len){
-			if(arr[i] == value){
+		while (i < len) {
+			if (arr[i] == value) {
 				count += 1;
 			}
+			i++;
 		}
 		return count;
 	}
 
-	public static char checkGrade(int marks){
-		if(marks <= 60)
+	public static char checkGrade(int marks) {
+		if (marks <= 60)
 			return 'D';
+		else if ((61 <= marks) && (marks <= 75))
+			return 'C';
+		else if ((76 <= marks) && (marks <= 90))
+			return 'B';
 		else
-			if((61 <= marks) || (marks <= 75))
-				return 'C';
-			else
-				if((76 <= marks) || (marks <= 90))
-					return 'B';
-				else
-					return 'A';
+			return 'A';
 	}
 
-	public static int countDigits(int num){
-		int count = 0;
-		while(num != 0){
-			num = num /10;
+	public static int countDigits(int num) {
+		int count = 0, tmp = num;
+		while (tmp > 0) {
+			tmp = tmp / 10;
 			count++;
 		}
 		return (num % count);
-
 	}
 
-	public static String printPattern(int n){
+	public static String printPattern(int n) {
 		int i, j, print = 1;
-		for(i = 1; i <= n; i++)
-			for(j=1;j<=2*i;j++)
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j <= 2 * i; j++) {
 				System.out.print((print));
+			}
 			System.out.println();
+		}
 
 
 		return " ";
--- src/test/java/com/jza/recruitment/AmazonTest.java
+++ src/test/java/com/jza/recruitment/AmazonTest.java
@@ -10,42 +10,42 @@ public class AmazonTest {
 	public void reverseArrayTest1() {
 		int[] input = {1, 2, 3, 4, 5, 6, 7};
 		int[] result = {7, 6, 5, 4, 3, 2, 1};
-		Assertions.assertEquals(result, Amazon.reverseArray(input));
+		Assertions.assertArrayEquals(result, Amazon.reverseArray(input));
 	}
 
 	@Test
 	public void reverseArrayTest2() {
-		int[] input = {2, 8, 4, 6};
+		int[] input = {6, 2, 4, 8};
 		int[] result = {8, 4, 2, 6};
-		Assertions.assertEquals(result, Amazon.reverseArray(input));
+		Assertions.assertArrayEquals(result, Amazon.reverseArray(input));
 	}
 
 	@Test
 	public void sortArrayTest1() {
 		int[] input = {23, 12, 14, 24, 21};
 		int[] result = {24, 23, 21, 14, 12};
-		Assertions.assertEquals(result, Amazon.sortArray(input));
+		Assertions.assertArrayEquals(result, Amazon.sortArray(input));
 	}
 
 	@Test
 	public void sortArrayTest2() {
 		int[] input = {1, 1, 1, 1, 1};
 		int[] result = {1, 1, 1, 1, 1};
-		Assertions.assertEquals(result, Amazon.sortArray(input));
+		Assertions.assertArrayEquals(result, Amazon.sortArray(input));
 	}
 
 	@Test
 	public void replaceValuesTest1() {
 		int[] input = {2, 5, 3, 7, 9, 4};
 		int[] result = {0, 0, 0, 0, 0, 0};
-		Assertions.assertEquals(result, Amazon.replaceValues(input));
+		Assertions.assertArrayEquals(result, Amazon.replaceValues(input));
 	}
 
 	@Test
 	public void replaceValuesTest2() {
 		int[] input = {21, 22, 3, 24, 65};
 		int[] result = {1, 1, 1, 1, 1,};
-		Assertions.assertEquals(result, Amazon.replaceValues(input));
+		Assertions.assertArrayEquals(result, Amazon.replaceValues(input));
 	}
 
 
