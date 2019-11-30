bf0b8416aea8acbcbae5d513264751297cce93d8
Jacek <jacekzalewski95@gmail.com>
2019-11-30 01:53:04 +0100

    published blackjack to kata

--- src/main/java/com/jza/codewars/BlackjackScorer.java
+++ src/main/java/com/jza/codewars/BlackjackScorer.java
@@ -6,37 +6,15 @@ import java.util.List;
 public class BlackjackScorer {
 
 	public int getBlackjackScore(List<String> cards) {
-		Integer sumWithoutAce = 0;
-		Integer acesCount = 0;
+		int sum = 0, aces = 0;
 		for (String card : cards) {
 			if (card.equals("A")) {
-				acesCount++;
-			} else {
-				sumWithoutAce += getValueOfCard(card);
+				aces++;
 			}
+			sum += getValueOfCard(card);
 		}
-		if (acesCount == 0) {
-			return sumWithoutAce;
-		}
-		if (sumWithoutAce + 11 * acesCount == 21) {
-			return 21;
-		}
-		if (sumWithoutAce + acesCount >= 21) {
-			return sumWithoutAce + acesCount;
-		}
-
-		int sum = sumWithoutAce;
-		int aces = acesCount;
-		for (int i = 0; i < acesCount; i++) {
-			sum++;
-			aces--;
-			if (sum + 1 >= 21 && aces == 0) {
-				return sum + aces;
-			}
-		}
-		aces = acesCount;
-		for (int i = 0; i < acesCount; i++) {
-			if (sum + 10 >= 21 || aces == 0) {
+		while (aces > 0) {
+			if (sum + 10 > 21) {
 				return sum;
 			}
 			sum += 10;
@@ -50,11 +28,9 @@ public class BlackjackScorer {
 			put("J", 10);
 			put("Q", 10);
 			put("K", 10);
-			put("A", 0);
+			put("A", 1);
 		}};
 		Integer value = cardValues.get(card);
 		return value != null ? value : Integer.parseInt(card);
 	}
-
-
 }
--- src/test/java/com/jza/codewars/BlackjackScorerTest.java
+++ src/test/java/com/jza/codewars/BlackjackScorerTest.java
@@ -1,10 +1,11 @@
 package com.jza.codewars;
 
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlackjackScorerTest {
 
@@ -12,92 +13,92 @@ public class BlackjackScorerTest {
 
 	@Test
 	public void test1() {
-		Assertions.assertEquals(5, blackjackScorer.getBlackjackScore(Arrays.asList("2", "3")));
+		assertEquals(5, blackjackScorer.getBlackjackScore(Arrays.asList("2", "3")));
 	}
 
 	@Test
 	public void test2() {
-		Assertions.assertEquals(22, blackjackScorer.getBlackjackScore(Arrays.asList("7", "7", "8")));
+		assertEquals(22, blackjackScorer.getBlackjackScore(Arrays.asList("7", "7", "8")));
 	}
 
 	@Test
 	public void test3() {
-		Assertions.assertEquals(19, blackjackScorer.getBlackjackScore(Arrays.asList("4", "7", "8")));
+		assertEquals(19, blackjackScorer.getBlackjackScore(Arrays.asList("4", "7", "8")));
 	}
 
 	@Test
 	public void test4() {
-		Assertions.assertEquals(30, blackjackScorer.getBlackjackScore(Arrays.asList("K", "J", "Q")));
+		assertEquals(30, blackjackScorer.getBlackjackScore(Arrays.asList("K", "J", "Q")));
 	}
 
 	@Test
 	public void test5() {
-		Assertions.assertEquals(14, blackjackScorer.getBlackjackScore(Arrays.asList("A", "3")));
+		assertEquals(14, blackjackScorer.getBlackjackScore(Arrays.asList("A", "3")));
 	}
 
 	@Test
 	public void test6() {
-		Assertions.assertEquals(12, blackjackScorer.getBlackjackScore(Arrays.asList("A", "A")));
+		assertEquals(12, blackjackScorer.getBlackjackScore(Arrays.asList("A", "A")));
 	}
 
 	@Test
 	public void test7() {
-		Assertions.assertEquals(22, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "A", "9", "9")));
+		assertEquals(22, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "A", "9", "9")));
 	}
 
 	@Test
 	public void test8() {
-		Assertions.assertEquals(25, blackjackScorer.getBlackjackScore(Arrays.asList("5", "4", "3", "2", "A", "K")));
+		assertEquals(25, blackjackScorer.getBlackjackScore(Arrays.asList("5", "4", "3", "2", "A", "K")));
 	}
 
 	@Test
 	public void test9() {
-		Assertions.assertEquals(18, blackjackScorer.getBlackjackScore(Arrays.asList("2", "3", "3", "9", "A")));
+		assertEquals(18, blackjackScorer.getBlackjackScore(Arrays.asList("2", "3", "3", "9", "A")));
 	}
 
 
 	@Test
 	public void test10() {
-		Assertions.assertEquals(30, blackjackScorer.getBlackjackScore(Arrays.asList("K", "J", "Q")));
+		assertEquals(30, blackjackScorer.getBlackjackScore(Arrays.asList("K", "J", "Q")));
 	}
 
 	@Test
 	public void test11() {
-		Assertions.assertEquals(46, blackjackScorer.getBlackjackScore(Arrays.asList("K", "Q", "K", "7", "9")));
+		assertEquals(46, blackjackScorer.getBlackjackScore(Arrays.asList("K", "Q", "K", "7", "9")));
 	}
 
 	@Test
 	public void test12() {
-		Assertions.assertEquals(13, blackjackScorer.getBlackjackScore(Arrays.asList("A", "4", "4", "4")));
+		assertEquals(13, blackjackScorer.getBlackjackScore(Arrays.asList("A", "4", "4", "4")));
 	}
 
 	@Test
 	public void test13() {
-		Assertions.assertEquals(15, blackjackScorer.getBlackjackScore(Arrays.asList("A", "5", "9")));
+		assertEquals(15, blackjackScorer.getBlackjackScore(Arrays.asList("A", "5", "9")));
 	}
 
 	@Test
 	public void test14() {
-		Assertions.assertEquals(13, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "3", "7")));
+		assertEquals(13, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "3", "7")));
 	}
 
 	@Test
 	public void test15() {
-		Assertions.assertEquals(15, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "3", "4", "5")));
+		assertEquals(15, blackjackScorer.getBlackjackScore(Arrays.asList("A", "2", "3", "4", "5")));
 	}
 
 	@Test
 	public void test16() {
-		Assertions.assertEquals(36, blackjackScorer.getBlackjackScore(Arrays.asList("K", "8", "9", "9")));
+		assertEquals(36, blackjackScorer.getBlackjackScore(Arrays.asList("K", "8", "9", "9")));
 	}
 
 	@Test
 	public void test17() {
-		Assertions.assertEquals(14, blackjackScorer.getBlackjackScore(Arrays.asList("A", "3", "10")));
+		assertEquals(14, blackjackScorer.getBlackjackScore(Arrays.asList("A", "3", "10")));
 	}
 
 	@Test
 	public void test18() {
-		Assertions.assertEquals(21, blackjackScorer.getBlackjackScore(Arrays.asList("A", "K")));
+		assertEquals(21, blackjackScorer.getBlackjackScore(Arrays.asList("A", "K")));
 	}
 }
