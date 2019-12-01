62deb4f242c857a7846841ff07e1a49b5d271fa8
Jacek <jacekzalewski95@gmail.com>
2019-12-01 23:20:31 +0100

    Protect trees - use stack, format final answse

--- src/main/java/com/jza/codewars/ProtectTrees.java
+++ src/main/java/com/jza/codewars/ProtectTrees.java
@@ -1,23 +1,29 @@
 package com.jza.codewars;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 public class ProtectTrees {
 
 	public String decompose(long n) {
-		List<Long> squares = new LinkedList<>();
+		Stack<Long> squares = new Stack<>();
 		Long number = n - 1;
 		Long leftOver = n * n;
 		while (leftOver > 0) {
 			Long square = number * number;
-			squares.add(number);
+			squares.push(number);
 			leftOver -= square;
 			number = (long) Math.sqrt(leftOver);
 		}
 		System.out.println(squares);
-		return "";
+		return convertAnswerFormat(squares);
 	}
 
-
+	private String convertAnswerFormat(Stack<Long> values) {
+		StringBuilder sb = new StringBuilder();
+		while (!values.isEmpty()) {
+			sb.append(values.pop());
+			sb.append(" ");
+		}
+		return sb.toString().trim();
+	}
 }
