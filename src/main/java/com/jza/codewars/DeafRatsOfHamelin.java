d209082103ab48cfc341a24d88a16aa0623fe9aa
tubidubidam <jacekzalewski95@gmail.com>
2019-12-06 12:12:21 +0100

    Deaf mice, one loop solution

--- src/main/java/com/jza/codewars/DeafRatsOfHamelin.java
+++ src/main/java/com/jza/codewars/DeafRatsOfHamelin.java
@@ -1,37 +1,22 @@
 package com.jza.codewars;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DeafRatsOfHamelin {
 
-	public  int countDeafRats(final String town) {
+	public int countDeafRats(final String town) {
 		String tightTown = town.replaceAll("\\s+", "");
-		char [] chars = tightTown.toCharArray();
-		List<Character> mice = new ArrayList<>();
-		int hamelingIndex = 0;
-		for (int i = 0; i < chars.length; i++) {
-			if(chars[i] == '~'){
-				mice.add('R');
-				i++;
-			} else if(chars[i] == 'O'){
-				mice.add('L');
-				i++;
-			} else if (chars[i] == 'P'){
-				mice.add('P');
-				hamelingIndex = i/2;
-			}
-		}
-		System.out.println(mice);
-
+		char[] chars = tightTown.toCharArray();
+		int hamelingIndex = chars.length - 1;
 		int deafMmice = 0;
-		for (int i = 0; i < mice.size(); i++) {
-			Character mouse  = mice.get(i);
-			if(i < hamelingIndex && mouse == 'L'){
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == '~' && i > hamelingIndex) {
 				deafMmice++;
-			} else if(i > hamelingIndex && mouse == 'R'){
+			} else if (chars[i] == 'O' && i < hamelingIndex) {
 				deafMmice++;
+			} else if (chars[i] == 'P') {
+				hamelingIndex = i;
+				i--;
 			}
+			i++;
 		}
 
 		return deafMmice;
