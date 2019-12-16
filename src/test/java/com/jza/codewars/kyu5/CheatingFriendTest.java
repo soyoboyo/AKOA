d6e1fa19b214a921b4311179b1c92eee130f0589
Jacek <jacekzalewski95@gmail.com>
2019-12-16 01:01:51 +0100

    CheatingFriend optimal solution

--- src/test/java/com/jza/codewars/kyu5/CheatingFriendTest.java
+++ src/test/java/com/jza/codewars/kyu5/CheatingFriendTest.java
@@ -12,15 +12,27 @@ class CheatingFriendTest {
 	@Test
 	public void test1() {
 		List<long[]> res = new ArrayList<long[]>();
-		res.add(new long[]{6, 7});
-		res.add(new long[]{7, 6});
+		res.add(new long[]{550320, 908566});
+		res.add(new long[]{908566, 550320});
+		res.add(new long[]{559756, 893250});
+		res.add(new long[]{893250, 559756});
 		List<long[]> a = CheatingFriend.isHeCheating(1000003);
 		assertArrayEquals(res.get(0), a.get(0));
 		assertArrayEquals(res.get(1), a.get(1));
 	}
 
 	@Test
-	public void test12() {
+	public void test2() {
+		List<long[]> res = new ArrayList<long[]>();
+		res.add(new long[]{28, 43});
+		res.add(new long[]{43, 28});
+		List<long[]> a = CheatingFriend.isHeCheating(50);
+		assertArrayEquals(res.get(0), a.get(0));
+		assertArrayEquals(res.get(1), a.get(1));
+	}
+
+	@Test
+	public void test3() {
 		List<long[]> res = new ArrayList<long[]>();
 		res.add(new long[]{15, 21});
 		res.add(new long[]{21, 15});
@@ -30,7 +42,7 @@ class CheatingFriendTest {
 	}
 
 	@Test
-	public void test14() {
+	public void test4() {
 		List<long[]> res = new ArrayList<long[]>();
 		List<long[]> a = CheatingFriend.isHeCheating(100);
 		assertTrue(res.size() == a.size());
