386538b9c31eff5f817375ef1b814c29319f2398
tubidubidam <jacekzalewski95@gmail.com>
2019-12-20 13:12:41 +0100

    ProtectTrees getting there

--- src/main/java/com/jza/codewars/ProtectTrees.java
+++ src/main/java/com/jza/codewars/ProtectTrees.java
@@ -1,29 +1,55 @@
 package com.jza.codewars;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 public class ProtectTrees {
 
-	public String decompose(long n) {
-		Stack<Long> squares = new Stack<>();
-		Long number = n - 1;
-		Long leftOver = n * n;
-		while (leftOver > 0) {
-			Long square = number * number;
-			squares.push(number);
-			leftOver -= square;
-			number = (long) Math.sqrt(leftOver);
-		}
-		System.out.println(squares);
-		return convertAnswerFormat(squares);
+	public static String decompose(long n) {
+		List<Long> solution = getSolution(n);
+		return convertAnswerFormat(solution);
 	}
 
-	private String convertAnswerFormat(Stack<Long> solution) {
+	private static List<Long> getSolution(long n) {
+		List<Long> solution = new Stack<>();
+		Long number = n - 1;
+		long leftOver = n * n;
+		long previousN = number;
+		do {
+			long square = number * number;
+			leftOver -= square;
+
+			solution.add(number);
+			number = (long) Math.sqrt(leftOver);
+
+		} while (leftOver > 0);
+		return solution;
+	}
+
+	private static boolean verifySolution(List<Long> solution) {
+		long previousNumber = solution.get(0);
+		for (int i = 1; i < solution.size(); i++) {
+			long currentNumber = solution.get(i);
+			if (currentNumber == previousNumber) {
+				return false;
+			}
+			previousNumber = currentNumber;
+		}
+		return true;
+	}
+
+	private static String convertAnswerFormat(List<Long> solution) {
+		Collections.reverse(solution);
 		StringBuilder sb = new StringBuilder();
-		while (!solution.isEmpty()) {
-			sb.append(solution.pop());
+		for (Long square : solution) {
+			sb.append(square);
 			sb.append(" ");
 		}
+		System.out.println(sb.toString().trim());
+		System.out.println(verifySolution(solution));
 		return sb.toString().trim();
 	}
+
 }
--- src/test/java/com/jza/codewars/ProtectTreesTest.java
+++ src/test/java/com/jza/codewars/ProtectTreesTest.java
@@ -5,53 +5,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProtectTreesTest {
-	ProtectTrees protectTrees = new ProtectTrees();
 
 	@Test
 	public void test1() {
-		long n = 11;
-		assertEquals("1 2 4 10", protectTrees.decompose(n));
+		assertEquals("1 2 4 10", ProtectTrees.decompose(11));
 	}
 
 	@Test
 	public void test2() {
-		long n = 50;
-		assertEquals("1 3 5 8 49", protectTrees.decompose(n));
+		assertEquals("1 3 5 8 49", ProtectTrees.decompose(50));
 	}
 
 	@Test
 	public void test3() {
-		long n = 2;
-		assertEquals(null, protectTrees.decompose(n));
+		assertEquals(null, ProtectTrees.decompose(2));
 	}
 
 	@Test
 	public void test4() {
-		long n = 15009;
-		assertEquals("1 2 3 5 7 173 15008", protectTrees.decompose(n));
+		assertEquals("1 2 3 5 7 173 15008", ProtectTrees.decompose(15009));
 	}
 
 	@Test
 	public void test5() {
-		long n = 18234;
-		assertEquals("1 2 3 8 17 190 18233", protectTrees.decompose(n));
+		assertEquals("1 2 3 8 17 190 18233", ProtectTrees.decompose(18234));
 	}
 
 	@Test
 	public void test6() {
-		long n = 17638;
-		assertEquals("1 4 17 187 17637", protectTrees.decompose(n));
+		assertEquals("1 4 17 187 17637", ProtectTrees.decompose(17638));
 	}
 
 	@Test
 	public void test7() {
-		long n = 9092;
-		assertEquals("3 7 13 134 9091", protectTrees.decompose(n));
+		assertEquals("3 7 13 134 9091", ProtectTrees.decompose(9092));
 	}
 
 	@Test
 	public void test8() {
-		long n = 8755;
-		assertEquals("2 9 132 8754", protectTrees.decompose(n));
+		assertEquals("2 9 132 8754", ProtectTrees.decompose(8755));
 	}
 }
