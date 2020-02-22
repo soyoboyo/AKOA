59bc087e166afd29a2b48f223588aa8cb4160b4a
tubidubidam <jacekzalewski95@gmail.com>
2020-02-22 15:33:37 +0100

    RainCapaicty update

--- src/main/java/com/jza/dip/uber/CapacityForRain.java
+++ src/main/java/com/jza/dip/uber/CapacityForRain.java
@@ -7,32 +7,52 @@ class CapacityForRain {
 		}
 		int capacity = 0;
 		int potentialCapacity = 0;
-		int leftEdge = landscape[0];
-		boolean isInGap = false;
-		int gapCount = 0;
+		boolean isInGap = landscape[1] < landscape[0];
+		int leftEdge = isInGap ? landscape[0] : landscape[1];
+		int gapSize = 0;
 		for (int i = 1; i < landscape.length; i++) {
 			int current = landscape[i];
-			if (current >= leftEdge && !isInGap) {
-				leftEdge = current;
-				gapCount = 0;
-			} else if (current < leftEdge && i != landscape.length - 1) {
-				isInGap = true;
-				potentialCapacity += leftEdge - current;
-				gapCount++;
-				System.out.println(gapCount);
-			} else if (isInGap) {
-				System.out.println(current + "  " + leftEdge);
-				if (current < leftEdge) {
-					capacity += (potentialCapacity - Math.min(current, leftEdge) * gapCount);
-				} else if (current > leftEdge) {
-					capacity += (potentialCapacity - Math.min(current, leftEdge) * (gapCount-1));
+			if (isInGap) {
+				if (current >= leftEdge) {
+					leftEdge = current;
+					isInGap = false;
 				} else {
-					capacity += potentialCapacity;
+					potentialCapacity += leftEdge - current;
+					gapSize++;
 				}
-				gapCount = 0;
+			} else {
+				if (current >= leftEdge) {
+					capacity += (potentialCapacity - (Math.min(current, leftEdge) * gapSize));
+					gapSize = 0;
+					potentialCapacity = 0;
+				} else {
+					isInGap = true;
+				}
 				leftEdge = current;
-				isInGap = false;
 			}
+
+
+//			if (current >= leftEdge && !isInGap) {
+//				leftEdge = current;
+//				gapCount = 0;
+//			} else if (current < leftEdge && i != landscape.length - 1) {
+//				isInGap = true;
+//				potentialCapacity += leftEdge - current;
+//				gapCount++;
+//				System.out.println(gapCount);
+//			} else if (isInGap) {
+//				System.out.println(current + "  " + leftEdge);
+//				if (current < leftEdge) {
+//					capacity += (potentialCapacity - Math.min(current, leftEdge) * gapCount);
+//				} else if (current > leftEdge) {
+//					capacity += (potentialCapacity - Math.min(current, leftEdge) * (gapCount-1));
+//				} else {
+//					capacity += potentialCapacity;
+//				}
+//				gapCount = 0;
+//				leftEdge = current;
+//				isInGap = false;
+//			}
 		}
 		return capacity;
 	}
